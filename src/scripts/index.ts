40cb01fd23146d9e2aff4971048deaf368c3aab5
Adam Joseph Cook <adam.j.cook@alliedstrand.com>
2018-07-24 06:43:16 -0500

    chore: add build scripts and dependencies
    Add Gulp and Webpack build scripts and package.json
    dependencies for the build and source file linting.

---
+++ src/scripts/index.ts
@@ -0,0 +1,7 @@
+/**
+ * Copyright 2018 SME DFW Chapter Contributors. All Rights Reserved.
+ * See LICENSE in the repository root for license information.
+ * =============================================================================
+ */
+
+import "../styles/styles.scss";
